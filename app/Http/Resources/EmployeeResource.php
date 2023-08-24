842fee961b137cd78c7a472cc4ee2216fc0c8312
PaingMinSoe <paingminsoe224@gmail.com>
2023-08-24 19:13:26 +0630

    EmployeeResource changes

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -16,6 +16,7 @@ class EmployeeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'phone_no' => $this->phone_no,
